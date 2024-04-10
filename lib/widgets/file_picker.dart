63f20487b8d5269def5591522617876608d1199c
wuocdat <quocdatpadpqn@gmail.com>
2024-04-11 01:55:16 +0700

    add loading effect when choosing file

--- lib/widgets/file_picker.dart
+++ lib/widgets/file_picker.dart
@@ -1,11 +1,13 @@
 import 'package:file_picker/file_picker.dart' as picker;
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:tctt_mobile/theme/colors.dart';
 import 'package:tctt_mobile/widgets/border_container.dart';
+import 'package:tctt_mobile/widgets/loader.dart';
 
 const int _maxFileSizeInMb = 10 * 1024 * 1024;
 
-class FilePicker extends StatelessWidget {
+class FilePicker extends StatefulWidget {
   const FilePicker({
     super.key,
     required List<picker.PlatformFile> fileNames,
@@ -15,18 +17,31 @@ class FilePicker extends StatelessWidget {
   final List<picker.PlatformFile> _files;
   final void Function(List<picker.PlatformFile>) onFilesSelected;
 
+  @override
+  State<FilePicker> createState() => _FilePickerState();
+}
+
+class _FilePickerState extends State<FilePicker> {
+  bool _isLoading = false;
+
   Future<void> _onTap() async {
+    setState(() {
+      _isLoading = true;
+    });
     picker.FilePickerResult? result =
         await picker.FilePicker.platform.pickFiles(allowMultiple: true);
 
     if (result != null) {
+      setState(() {
+        _isLoading = false;
+      });
       final files = result.files.nonNulls
           .where((element) => element.size <= _maxFileSizeInMb)
           .toList();
       if (files.length > 5) {
         files.removeRange(5, files.length);
       }
-      onFilesSelected(result.files.nonNulls
+      widget.onFilesSelected(result.files.nonNulls
           .where((element) => element.size <= _maxFileSizeInMb)
           .toList());
     }
@@ -39,34 +54,37 @@ class FilePicker extends StatelessWidget {
       child: BorderContainer(
         child: Padding(
           padding: const EdgeInsetsDirectional.fromSTEB(16, 12, 16, 12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Icon(
-                    Icons.attach_file_rounded,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                  const SizedBox(width: 16),
-                  const Expanded(
-                    child: Text(
-                      'Đính kèm file (kích thước nhỏ hơn 10MB, tối đa 5 file)',
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
+          child: _isLoading
+              ? const LoaderWithMessage(message: "Đang mở trình duyệt file...")
+              : Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Icon(
+                          Icons.attach_file_rounded,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        const SizedBox(width: 16),
+                        const Expanded(
+                          child: Text(
+                            'Đính kèm file (kích thước nhỏ hơn 10MB, tối đa 5 file)',
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-              _files.isNotEmpty
-                  ? const Divider(
-                      color: AppColors.secondaryBackground,
-                    )
-                  : const SizedBox(),
-              ..._files.map((e) => FileItem(fileName: e.name, size: e.size)),
-            ],
-          ),
+                    widget._files.isNotEmpty
+                        ? const Divider(
+                            color: AppColors.secondaryBackground,
+                          )
+                        : const SizedBox(),
+                    ...widget._files
+                        .map((e) => FileItem(fileName: e.name, size: e.size)),
+                  ],
+                ),
         ),
       ),
     );
--- lib/widgets/loader.dart
+++ lib/widgets/loader.dart
@@ -10,3 +10,34 @@ class Loader extends StatelessWidget {
     return const Center(child: CircularProgressIndicator());
   }
 }
+
+class LoaderWithMessage extends StatelessWidget {
+  const LoaderWithMessage({
+    super.key,
+    required this.message,
+  });
+
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(message),
+          const SizedBox(
+            width: 12,
+          ),
+          const SizedBox(
+            width: 20,
+            height: 20,
+            child: CircularProgressIndicator(
+              strokeWidth: 2,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
