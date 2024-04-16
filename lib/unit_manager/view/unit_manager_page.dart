605ed6cad5816f21c2fb13dc56e3b7e051ab49af
dt140120 <dong.tq2000@gmail.com>
2024-04-16 09:14:48 +0700

    update unit manager page

--- lib/unit_manager/view/unit_manager_page.dart
+++ lib/unit_manager/view/unit_manager_page.dart
@@ -1,9 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:tctt_mobile/theme/colors.dart';
-import 'package:tctt_mobile/widgets/border_container.dart';
+import 'package:tctt_mobile/account_setting/view/account_setting_page.dart';
 
-class CustomButton extends StatelessWidget {
-  const CustomButton({
+class CustomPadding extends StatelessWidget {
+  const CustomPadding({
+    super.key,
+    required String text,
+    required VoidCallback? onPressed,
+  }) : _text = text,
+       _onPressed = onPressed;
+
+  final VoidCallback? _onPressed;
+  final String _text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 1),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 0,
+              color: Color(0xFFE0E3E7),
+              offset: Offset(0.0, 1),
+            )
+          ],
+          borderRadius: BorderRadius.circular(0),
+          shape: BoxShape.rectangle,
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                width: 4,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(2),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(12, 0, 0, 0),
+                  child: Text(
+                    _text,
+                    style: TextStyle(
+                      fontFamily: 'Plus Jakarta Sans',
+                      letterSpacing: 0,
+                    ),
+                  ),
+                ),
+              ),
+              Icon(
+                Icons.more_vert,
+                color: Colors.black,
+                size: 24,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WhiteCustomtButton extends StatelessWidget {
+  const WhiteCustomtButton({
     super.key,
     required String text,
     required IconData icon,
@@ -20,41 +87,36 @@ class CustomButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.centerLeft,
-      child: ElevatedButton(
-        onPressed:() {},
-        style: ElevatedButton.styleFrom(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          elevation: 6,
-          textStyle: const TextStyle(fontSize: 13, fontFamily: 'Plus Jakarta Sans', letterSpacing: 0, fontWeight: FontWeight.bold),
-          backgroundColor: AppColors.primary,
-          foregroundColor: Colors.white,
-          minimumSize: const Size(0, 30),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-        ),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(
-              _icon,
-              size: 16,
-            ),
-            const SizedBox(width: 6),
-            Text(_text),
-          ],
+      child: Padding(
+        padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            elevation: 6,
+            textStyle: const TextStyle(
+                fontSize: 13,
+                fontFamily: 'Plus Jakarta Sans',
+                letterSpacing: 0,
+                fontWeight: FontWeight.bold),
+            backgroundColor: Colors.white,
+            foregroundColor: AppColors.primary,
+            minimumSize: const Size(0, 28),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                _icon,
+                size: 16,
+              ),
+              const SizedBox(width: 6),
+              Text(_text),
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
-
-class Devider extends StatelessWidget {
-  const Devider({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Divider(
-      thickness: 1,
-      color: AppColors.secondaryBackground,
     );
   }
 }
@@ -65,157 +127,63 @@ class UnitManager extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            automaticallyImplyLeading: false,
-            leading: const BackButton(
-              color: Colors.black,
-            ),
-            title: const Text(
-              'Quản lí tài khoản',
-              style: TextStyle(
-                fontFamily: 'Urbanest',
-                letterSpacing: 0,
+        home: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              automaticallyImplyLeading: false,
+              leading: const BackButton(
+                color: Colors.black,
+              ),
+              title: const Text(
+                'Danh bạ đơn vị',
+                style: TextStyle(
+                  fontFamily: 'Urbanest',
+                  letterSpacing: 0,
+                ),
               ),
             ),
-          ),
-          body: SafeArea(
-            top: true,
-            child: Padding(
-              padding: const EdgeInsetsDirectional.fromSTEB(16, 12, 16, 0),
-              child: SingleChildScrollView(
-                child: Column(
+            body: SafeArea(
+              child: Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(12, 0, 12, 0),
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  scrollDirection: Axis.vertical,
                   children: [
-                    BorderContainer(
-                      child: Align(
-                        alignment: const AlignmentDirectional(0, 0),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8),
-                          child: Column(
-                            children: [
-                              const Padding(
-                                padding: EdgeInsets.all(8),
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.max,
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Expanded(
-                                      child: Padding(
-                                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 12, 0),
-                                        child: Text(
-                                          'Thông tin cá nhân',
-                                          style: TextStyle(
-                                            fontFamily: 'Outfit',
-                                            color: Color(0xFF14181B),
-                                            fontSize: 24,
-                                            letterSpacing: 0,
-                                            fontWeight: FontWeight.w500,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              CustomButton(text: 'Cập nhật ', icon: Icons.edit_square, onPressed: () {}),
-                            ],
-                          ),
+                    const Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 12),
+                      child: Text(
+                        'Thành phố Đà Nẵng',
+                        style: TextStyle(
+                          fontFamily: 'Plus Jakarta Sans',
+                          fontSize: 16,
+                          letterSpacing: 0,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(0, 16, 0, 0),
-                      child: BorderContainer(
-                        child: Align(
-                          alignment: AlignmentDirectional(0, 0),
-                          child: Padding(
-                            padding: EdgeInsets.all(8),
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.all(8),
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.max,
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Expanded(
-                                        child: Padding(
-                                          padding: EdgeInsetsDirectional.fromSTEB(
-                                              0, 0, 12, 0),
-                                          child: Text(
-                                            'Bảo mật',
-                                            style: TextStyle(
-                                              fontFamily: 'Outfit',
-                                              color: Color(0xFF14181B),
-                                              fontSize: 24,
-                                              letterSpacing: 0,
-                                              fontWeight: FontWeight.w500,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Devider(),
-                                CustomButton(text: 'Thay đổi mật khẩu', icon: Icons.password_rounded, onPressed: () {}),
-                              ],
-                            ),
-                          ),
-                        ),
+                      padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 12, 12),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          WhiteCustomtButton(text: 'Thành viên', icon: Icons.group, onPressed: () {}),
+                          SizedBox(width: 16),
+                          CustomButton(text: 'Thêm đơn vị', icon: Icons.add, onPressed: () {}),
+                        ],
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(0, 16, 0, 0),
-                      child: BorderContainer(
-                        child: Align(
-                          alignment: AlignmentDirectional(0, 0),
-                          child: Padding(
-                            padding: EdgeInsets.all(8),
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.all(8),
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.max,
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Expanded(
-                                        child: Padding(
-                                          padding: EdgeInsetsDirectional.fromSTEB(
-                                              0, 0, 12, 0),
-                                          child: Text(
-                                            'Xác thực 2 lớp',
-                                            style: TextStyle(
-                                              fontFamily: 'Outfit',
-                                              color: Color(0xFF14181B),
-                                              fontSize: 24,
-                                              letterSpacing: 0,
-                                              fontWeight: FontWeight.w500,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Devider(),
-                                CustomButton(text: 'Ngừng kích hoạt', icon: Icons.privacy_tip_outlined, onPressed: () {}),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
+                    CustomPadding(text: 'Quận Hải  Châu', onPressed: () {} ),
+                    CustomPadding(text: 'Quận Ngũ Hành Sơn', onPressed: () {} ),
+                    CustomPadding(text: 'Quận Cẩm Lệ', onPressed: () {} ),
+                    CustomPadding(text: 'Quận Liên Chiểu', onPressed: () {} )
                   ],
                 ),
               ),
             ),
           ),
-        ),
-      ),
+        )
     );
   }
 }
