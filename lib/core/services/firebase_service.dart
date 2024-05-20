4a498f55120a3aebae0711d3e98fe73d4aa8c190
wuocdat <quocdatpadpqn@gmail.com>
2024-05-20 07:48:52 +0700

    refactored code

--- lib/core/services/firebase_service.dart
+++ lib/core/services/firebase_service.dart
@@ -1,8 +1,7 @@
-import 'dart:developer';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:tctt_mobile/core/utils/logger.dart';
 import 'package:tctt_mobile/firebase_options.dart';
 import 'package:tctt_mobile/shared/enums.dart';
 
@@ -34,14 +33,14 @@ class FirebaseService {
       sound: true,
     );
 
-    log(settings.authorizationStatus.name);
+    logger.i(settings.authorizationStatus.name);
 
     await _handleMessagesFromFCM();
   }
 
   static Future<String?> getFCMToken() async {
     final fcmToken = await FirebaseMessaging.instance.getToken();
-    log('FCM Token: $fcmToken');
+    logger.i('FCM Token: $fcmToken');
 
     return fcmToken;
   }
