7ebcf65220e7c437a954a654794b4e9a90df5a44
wuocdat <quocdatpadpqn@gmail.com>
2024-05-21 21:49:07 +0700

    update login page

--- lib/features/login/view/login_form.dart
+++ lib/features/login/view/login_form.dart
@@ -10,7 +10,8 @@ class LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<LoginBloc, LoginState>(
+    return BlocConsumer<LoginBloc, LoginState>(
+      listenWhen: (previous, current) => previous.status != current.status,
       listener: (context, state) {
         if (state.status.isFailure) {
           ScaffoldMessenger.of(context)
@@ -20,19 +21,30 @@ class LoginForm extends StatelessWidget {
             );
         }
       },
-      child: Align(
-        alignment: const Alignment(0, -1 / 3),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            _UsernameInput(),
-            const Padding(padding: EdgeInsets.all(12)),
-            _PasswordInput(),
-            const Padding(padding: EdgeInsets.all(12)),
-            _LoginButton(),
-          ],
-        ),
-      ),
+      builder: (context, state) {
+        return Align(
+          alignment: const Alignment(0, -1 / 3),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _UsernameInput(),
+              const Padding(padding: EdgeInsets.all(12)),
+              _PasswordInput(),
+              Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: Row(
+                  children: [
+                    Checkbox(value: true, onChanged: (newValue) {}),
+                    const Text('Nhớ mật khẩu?')
+                  ],
+                ),
+              ),
+              const Padding(padding: EdgeInsets.all(8)),
+              _LoginButton(),
+            ],
+          ),
+        );
+      },
     );
   }
 }
@@ -78,16 +90,15 @@ class _LoginButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<LoginBloc, LoginState>(
-      builder: (context, state) => state.status.isInProgress
-          ? const CircularProgressIndicator()
-          : ContainedButton(
-              text: "Đăng nhập",
-              onPressed: state.isValid
-                  ? () {
-                      context.read<LoginBloc>().add(const LoginSubmitted());
-                    }
-                  : null,
-            ),
+      builder: (context, state) => ContainedButton(
+        loading: state.status.isInProgress,
+        text: "Đăng nhập",
+        onPressed: state.isValid
+            ? () {
+                context.read<LoginBloc>().add(const LoginSubmitted());
+              }
+            : null,
+      ),
     );
   }
 }
